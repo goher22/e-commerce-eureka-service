425daa5c028402feb032087890c437d6be57c083
Carlos <gohercarlos@gmail.com>
2024-05-26 17:59:31 -0500

    init proyect

---
+++ src/main/java/com/code/whiskers/mic_e_commerce_eureka_services/MicECommerceEurekaServicesApplication.java
@@ -0,0 +1,13 @@
+package com.code.whiskers.mic_e_commerce_eureka_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicECommerceEurekaServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicECommerceEurekaServicesApplication.class, args);
+	}
+
+}
